bc0a9be03aa9305b549ff80f31c555b7ef0a5b6e
TheWalkingCat <thewalkingcat900@gmail.com>
2024-10-18 06:01:21 +0300

    Fix install_miners.sh

--- installation/install_miners.sh
+++ installation/install_miners.sh
@@ -17,4 +17,4 @@ ALEO_VERSION="3.0.10"
 mkdir -p $QUBIC_ALEO_FOLDER
 wget -P /tmp https://dl.qubic.li/downloads/qli-Client-$QUBICLI_VERSION-Linux-x64.tar.gz && wget -P /tmp https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/aleominer-$ALEO_VERISON.tar.gz
 tar -xvf /tmp/qli-Client-$QUBICLI_VERSION-Linux-x64.tar.gz -C $QUBIC_ALEO_FOLDER
-tar -xvf /tmp/aleominer-$ALEO_VERISON.tar.gz -C $QUBIC_ALEO_FOLDER
+tar -xvf /tmp/aleominer-$ALEO_VERSION.tar.gz -C $QUBIC_ALEO_FOLDER
