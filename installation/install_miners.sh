201608b9b2e0d762a98b3b4911a42e71ac2d2352
TheWalkingCat <thewalkingcat900@gmail.com>
2024-10-18 06:13:47 +0300

    Fix2

--- installation/install_miners.sh
+++ installation/install_miners.sh
@@ -16,7 +16,7 @@ ALEO_VERSION="3.0.10"
 # Install Qubic li + Aleo F2pool
 mkdir -p $QUBIC_ALEO_FOLDER
 wget -P /tmp https://dl.qubic.li/downloads/qli-Client-$QUBICLI_VERSION-Linux-x64.tar.gz					# Download Qubic li
-wget -P /tmp https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/aleominer-$ALEO_VERISON.tar.gz	# Download Aleominer
+wget -P /tmp https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/aleominer-$ALEO_VERSION.tar.gz	# Download Aleominer
 
 tar -xvf /tmp/qli-Client-$QUBICLI_VERSION-Linux-x64.tar.gz -C $QUBIC_ALEO_FOLDER	# Unarchived
 tar -xvf /tmp/aleominer-$ALEO_VERSION.tar.gz -C $QUBIC_ALEO_FOLDER			# Unarchived
